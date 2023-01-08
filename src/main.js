50ef6873e19f2ce138416330c51a34e565be772d
MiguelAngelGV17 <mgonzalez94@hotmail.es>
2023-01-08 18:05:56 -0500

    add responsive products with grid template columns

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-
-let foods = [
+{
+    
+let productsClothes = [
     {
         id: "0",
         name: "Hoodies",
@@ -28,7 +29,7 @@ const products = document.querySelector(".products");
 function printProducts() {
     let html = "";
 
-    foods.forEach(function ({ id, name, price, stock, urlImage }) {
+    productsClothes.forEach(function ({ id, name, price, stock, urlImage }) {
         html += `
             <div class="product">
                 <div class="product__img">
@@ -53,3 +54,46 @@ function printProducts() {
 }
 
 printProducts();
+}
+
+
+// let hoddies = [
+//     {
+//         id: "0",
+//         name: "Hoodies",
+//         price: 14,
+//         stock: 10,
+//         urlImage: "./src/img/featured1.png",
+//     },
+// ];
+
+// const products = document.querySelector(".hoddies");
+
+// function printProducts() {
+//     let html = "";
+
+//     hoddies.forEach(function ({ id, name, price, stock, urlImage }) {
+//         html += `
+//             <div class="product">
+//                 <div class="product__img">
+//                 <div>
+//                 <img src="${urlImage}" alt="${name}" />
+//                 </div>
+//                 </div>
+
+//                 <div class="product__info">
+//                     <h2>$ ${price}.00 | <span>Stock: ${stock}</span></h2>
+//                     <h3>${name}</h3>
+//                 </div>
+
+//                 <div class="product__options" id="${id}">
+//                     <button class="btnplus btn__add">+</button>
+//                 </div>
+//             </div>
+//         `;
+//     });
+
+//     products.innerHTML = html;
+// }
+
+// printProducts();
